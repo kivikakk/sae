d712775d5d67806aad34b45b58eadafe20cec9df
Amelia Cuss <amelia@kivikakk.ee>
2024-02-27 13:13:40 +1100

    use buffered fifo to use BRAM.

--- sae/rtl/uart/__init__.py
+++ sae/rtl/uart/__init__.py
@@ -1,5 +1,5 @@
 from amaranth import Module
-from amaranth.lib.fifo import SyncFIFO
+from amaranth.lib.fifo import SyncFIFOBuffered
 from amaranth.lib.io import Pin
 from amaranth.lib.wiring import Component, In, Out
 from amaranth_stdio.serial import AsyncSerial
@@ -17,7 +17,8 @@ class UART(Component):
 
     _plat_uart: Pin
     _baud: int
-    _fifo: SyncFIFO
+    _tx_fifo: SyncFIFOBuffered
+    _rx_fifo: SyncFIFOBuffered
 
     def __init__(self, plat_uart, baud=9600):
         self._plat_uart = plat_uart
@@ -28,53 +29,56 @@ class UART(Component):
         m = Module()
 
         if platform.simulation:
+            # Blackboxed in tests.
             return m
 
-        freq = getattr(platform, "default_clk_frequency", 1e6)
-
-        m.submodules.fifo = self._fifo = SyncFIFO(width=8, depth=32)
-        m.d.comb += [
-            self._fifo.w_data.eq(self.wr_data),
-            self._fifo.w_en.eq(self.wr_en),
-        ]
+        freq = platform.default_clk_frequency
 
         m.submodules.serial = serial = AsyncSerial(
-            divisor=int(freq // self._baud), pins=self._plat_uart
+            divisor=int(freq // self._baud),
+            pins=self._plat_uart,
         )
 
         # tx
+        m.submodules.tx_fifo = self._tx_fifo = SyncFIFOBuffered(width=8, depth=32)
+        m.d.comb += [
+            self._tx_fifo.w_data.eq(self.wr_data),
+            self._tx_fifo.w_en.eq(self.wr_en),
+        ]
         with m.FSM() as fsm:
             with m.State("idle"):
-                with m.If(serial.tx.rdy & self._fifo.r_rdy):
-                    m.d.sync += serial.tx.data.eq(self._fifo.r_data)
+                with m.If(serial.tx.rdy & self._tx_fifo.r_rdy):
+                    m.d.sync += serial.tx.data.eq(self._tx_fifo.r_data)
                     m.next = "wait"
 
             with m.State("wait"):
-                # actually need this lol
                 m.next = "idle"
 
             m.d.comb += [
                 serial.tx.ack.eq(fsm.ongoing("wait")),
-                self._fifo.r_en.eq(fsm.ongoing("wait")),
+                self._tx_fifo.r_en.eq(fsm.ongoing("wait")),
             ]
 
         # rx
+        m.submodules.rx_fifo = self._rx_fifo = SyncFIFOBuffered(width=8, depth=32)
+        m.d.comb += [
+            self.rd_rdy.eq(self._rx_fifo.r_rdy),
+            self.rd_data.eq(self._rx_fifo.r_data),
+            self._rx_fifo.r_en.eq(self.rd_en),
+        ]
         with m.FSM() as fsm:
             with m.State("idle"):
+                m.d.sync += self._rx_fifo.w_en.eq(0)
                 with m.If(serial.rx.rdy):
                     m.next = "read"
 
             with m.State("read"):
-                m.d.sync += self.rd_data.eq(serial.rx.data)
-                m.next = "consume"
+                m.d.sync += [
+                    self._rx_fifo.w_data.eq(serial.rx.data),
+                    self._rx_fifo.w_en.eq(1),
+                ]
+                m.next = "idle"
 
-            with m.State("consume"):
-                with m.If(self.rd_en):
-                    m.next = "idle"
-
-            m.d.comb += [
-                serial.rx.ack.eq(fsm.ongoing("idle")),
-                self.rd_rdy.eq(fsm.ongoing("consume")),
-            ]
+            m.d.comb += serial.rx.ack.eq(fsm.ongoing("idle"))
 
         return m
