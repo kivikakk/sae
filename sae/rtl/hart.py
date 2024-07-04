f0b79c4baf16e2b66d8629a1c833dde09ef2dc81
Asherah Connor <ashe@kivikakk.ee>
2024-07-04 17:49:02 +0300

    remove another. 2747 / 2186.

--- sae/rtl/hart.py
+++ sae/rtl/hart.py
@@ -435,14 +435,6 @@ class Hart(Elaboratable):
                     ]
                     m.next = "fetch.init"
 
-            with m.State("lh.wait"):
-                with m.If(mmu.read.resp_valid):
-                    m.d.sync += [
-                        self.xwr_en.eq(1),
-                        self.xwr_val.eq(mmu.read.resp_payload[:16].as_signed()),
-                    ]
-                    m.next = "fetch.init"
-
             with m.State("lhu.wait"):
                 with m.If(mmu.read.resp_valid):
                     m.d.sync += [
